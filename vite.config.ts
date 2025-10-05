c8e46975ae8f31bb60ee2b5237ffc0cbf55c0ba8
yoel 117 <mryexs117@gmail.com>
2025-10-04 23:45:42 -0600

    Configuración inicial de la PWA con manifest y service worker

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,31 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+     
+      manifest: {
+        name: 'Mi Primera PWA con Vite',
+        short_name: 'MiPWA',
+        description: 'Un proyecto PWA para la clase.',
+        theme_color: '#3367D6',
+        icons: [
+          {
+            src: 'icons/icons/pngwing.com (2).png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'icons/pngwing.com (1).png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
 })
